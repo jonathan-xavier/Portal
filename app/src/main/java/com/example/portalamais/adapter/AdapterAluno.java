e93e10718b70e339db5fa7953283b14239c220f7
Aluno <aluno@expert.local>
2019-11-28 12:55:14 -0300

    ultimas att em instituicao

---
+++ app/src/main/java/com/example/portalamais/adapter/AdapterAluno.java
@@ -0,0 +1,58 @@
+package com.example.portalamais.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.portalamais.R;
+import com.example.portalamais.model.Aluno;
+import com.example.portalamais.R;
+
+public class AdapterAluno extends RecyclerView.Adapter<AdapterAluno.MyViewHolder> {
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.adapter_lista_aluno, parent, false
+        );
+
+        return new MyViewHolder(itemLista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Aluno aluno = new Aluno();
+        holder.idAluno.setText(aluno.getIdAluno());
+        holder.nomeAluno.setText(aluno.getNomeAluno());
+        holder.labelIdAluno.getText();
+        holder.labelNomeAluno.getText();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 1;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView idAluno;
+        TextView nomeAluno;
+        TextView labelIdAluno;
+        TextView labelNomeAluno;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            idAluno = itemView.findViewById(R.id.textViewMatriculaAluno);
+            nomeAluno = itemView.findViewById(R.id.textViewNomeAluno);
+            labelIdAluno = itemView.findViewById(R.id.textViewLabelIdAluno);
+            labelNomeAluno = itemView.findViewById(R.id.textViewLabelNomeAluno);
+        }
+    }
+
+}
