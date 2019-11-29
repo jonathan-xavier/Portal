bf8d12b529bde604375001af77e9c6fed60d8573
Aluno <aluno@expert.local>
2019-11-29 09:24:15 -0300

    Alteracao

--- app/src/main/java/com/example/portala/activity/EventoInstituicaoActivity.java
+++ app/src/main/java/com/example/portala/activity/EventoInstituicaoActivity.java
@@ -11,6 +11,6 @@ public class EventoInstituicaoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_evento);
+        setContentView(R.layout.activity_evento_instituicao);
     }
 }
